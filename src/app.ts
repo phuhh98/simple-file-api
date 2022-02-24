60f107bd1a066b1f9c524e65a80fc61d52f3953f
Phu Huynh <phu_huynh@epam.com>
2022-02-24 11:20:22 +0700

    Reinstall chalk@4.1.0 to work with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import chalk from "chalk";
 
 const app = express();
-const port = 3000;
+const PORT = 8080;
 
-app.listen(port, () => {
-  console.log(`Timezones by location application is running on port ${port}.`);
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "okay" }).end();
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `\nExpress server has started at port ${chalk.bold.italic.yellow(PORT)}`
+  );
 });
